fa4be83fe680a7e658efaa93efe69cfcc43a420b
Jeremy <jeremy@kilsar.com>
2025-03-05 16:09:55 -0500

    Add in nested serializers and seializermethods

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
-from api.serializers import ProductSerializer
-from api.models import Product
+from api.serializers import OrderSerializer, ProductSerializer
+from api.models import Order, OrderItem, Product
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -12,14 +12,18 @@ def product_list(request):
     # many=True for multiple objects
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
-    # return JsonResponse({
-    #     'data': serializer.data
-    # })
     
 
 @api_view(['GET'])
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     serializer = ProductSerializer(product)
+    return Response(serializer.data)
+   
+
+@api_view(['GET'])
+def order_list(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
     return Response(serializer.data)
    
