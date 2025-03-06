6a3370e2d37dc76addddb8ace02b779e9842b656
Jeremy <jeremy@kilsar.com>
2025-03-06 10:47:27 -0500

    05-genericviews-listapiview-retrieveapiview

--- api/views.py
+++ api/views.py
@@ -5,31 +5,22 @@ from api.serializers import OrderSerializer, ProductSerializer, ProductInfoSeria
 from api.models import Order, OrderItem, Product
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import generics
 
-# function based views
-@api_view(['GET'])
-def product_list(request):
-    products = Product.objects.all()
-    # many=True for multiple objects
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+
+class ProductListAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.filter(stock__gt=0)
+    serializer_class = ProductSerializer
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # lookup_url_kwarg = 'product_id'
     
+class OrderListAPIView(generics.ListCreateAPIView):
+    queryset = Order.objects.prefetch_related('items__product').all()
+    serializer_class = OrderSerializer
 
-@api_view(['GET'])
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
-   
-
-@api_view(['GET'])
-def order_list(request):
-    orders = Order.objects.prefetch_related(
-        'items__product'
-        ).all()
-    serializer = OrderSerializer(orders, many=True)
-    return Response(serializer.data)
-   
 
 @api_view(['GET'])
 def product_info(request):
